3ac1d2c3c18b20ba41b2fe5495408b59b32179bc
paul <paulsinterniklaas@hotmail.com>
2020-07-05 21:13:46 +0200

    simpel dobbelsteentje gemaakt als test.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var random = new Random();
+            //int aantalOgen = random.Next(1, 7);
+            //int i = 0;
+
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+                Gooi();
+            }
+            while (keyinfo.Key != ConsoleKey.Escape);
+
         }
+
+        public static void Gooi()
+        {
+            var random = new Random();
+            int aantalOgen = random.Next(1, 7);
+            Console.WriteLine(aantalOgen);
+        }
+
     }
 }
